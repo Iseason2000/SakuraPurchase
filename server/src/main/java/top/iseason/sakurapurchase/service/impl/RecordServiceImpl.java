6916b10f87d78511fc3d7ae281ba49dbf0f392f2
Iseason2000 <Iseason2000@qq.com>
2022-10-20 14:26:01 +0800

    定时更新缓存

--- server/src/main/java/top/iseason/sakurapurchase/service/impl/RecordServiceImpl.java
+++ server/src/main/java/top/iseason/sakurapurchase/service/impl/RecordServiceImpl.java
@@ -4,6 +4,10 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lly835.bestpay.enums.OrderStatusEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import top.iseason.sakurapurchase.entity.Record;
 import top.iseason.sakurapurchase.entity.Stat;
@@ -17,6 +21,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.DoubleAdder;
 
 @Slf4j
+@EnableScheduling
+@EnableAsync
 @Service
 public class RecordServiceImpl extends ServiceImpl<RecordMapper, Record> implements RecordService {
     private DoubleAdder totalPaidAmount = null;
@@ -108,4 +114,34 @@ public class RecordServiceImpl extends ServiceImpl<RecordMapper, Record> impleme
         }
         return b;
     }
+
+    /**
+     * 每小时更新
+     */
+    @Async
+    @Scheduled(cron = "0 * 0/1 * * ?")
+    void updatePerHour() {
+        daily = new TotalStat(getHours(24));
+
+        totalPaidCount = new AtomicInteger(baseMapper.getTotalPaidCount());
+
+        DoubleAdder doubleAdder = new DoubleAdder();
+        Double amount = baseMapper.getTotalPaidAmount();
+        if (amount == null) amount = 0.0;
+        doubleAdder.add(amount);
+        totalPaidAmount = doubleAdder;
+        log.info("数据已更新: daily、totalPaidCount、totalPaidAmount");
+    }
+
+    /**
+     * 每天凌晨4点更新
+     */
+    @Async
+    @Scheduled(cron = "0 0 4 * * ?")
+    void updatePerDay() {
+        weekly = new TotalStat(getAWeek(0));
+        yearly = new TotalStat(getMonth(12));
+        log.info("数据已更新: weekly、yearly");
+    }
+
 }
